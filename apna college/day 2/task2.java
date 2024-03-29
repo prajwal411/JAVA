aaa7a1130a3a2dabf2dc3fd4b37d6c9b2e738539
prajwal411 <138669849+prajwal411@users.noreply.github.com>
2024-03-29 12:38:12 +0530

    Delete apna college directory

--- apna college/day 2/task2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class task2{
-    public static void main(String arg[]){
-        Scanner sc = new Scanner(System.in);
-        float Input = sc.nextFloat();
-        float Area = 3.14f * Input * Input;
-        System.out.print(Area);
-    }
-}
